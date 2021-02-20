08d76c231ea32f10ad13318d079cee8b59427bb9
Sashen <sashen50@gmail.com>
2021-02-20 12:28:45 +0200

    added code structure of mouse mover

---
+++ MouseMover/AppHosting.WinSvc.MouseMover/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using Microsoft.Extensions.Configuration;
+using NLog.Web;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppHosting.WinSvc.MouseMover
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      // https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-3
+      var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+      try
+      {
+        CreateHostBuilder(args).Build().RunAsync();
+      }
+      catch (Exception exception)
+      {
+        logger.Error(exception, "Stopped program because of exception");
+        throw;
+      }
+      finally
+      {
+        // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+        NLog.LogManager.Shutdown();
+      }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder()
+          .ConfigureServices((hostContext, services) =>
+          {
+            services.AddHostedService<Worker>();
+          })
+          .UseWindowsService()
+          .ConfigureLogging((hostBuilderContext, logging) =>
+          {
+            logging.ClearProviders(); //remove all logging provider first because by default the framework will add Console,Debug, TraceSource and EventSource providers
+            logging.SetMinimumLevel(LogLevel.Debug);
+            logging.AddEventLog(hostBuilderContext.Configuration.GetSection("Logging:EventLog").Get<EventLogSettings>());
+          }).UseNLog();  // NLog: Setup NLog for Dependency injection;
+  }
+}
---
+++ MouseMover/Service.MouseMover/MouseMover.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service.MouseMover
+{
+  public class MouseMover : IHostedService
+  {
+    private readonly ILogger<MouseMover> _logger;
+
+    public MouseMover(ILogger<MouseMover> logger)
+    {
+      _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+}
